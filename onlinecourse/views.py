a3a1bf59147473aec85cf9fea39d4affc3f58986
VALENTYN <lazepkav@gmail.com>
2023-07-27 11:07:16 -0400

    Build Views and Templates to Submit, Display, and Evaluate Exam Results

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 # <HINT> Import any new Models here
-from .models import Course, Enrollment
+from .models import Course, Enrollment, Question, Choice, Submission
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
@@ -110,18 +110,40 @@ def enroll(request, course_id):
          # Collect the selected choices from exam form
          # Add each selected choice object to the submission object
          # Redirect to show_exam_result with the submission id
-#def submit(request, course_id):
+def submit(request, course_id):
+    user = request.user
+    course = get_object_or_404(Course, pk=course_id)
+
+    is_enrolled = check_if_enrolled(user, course)
+    if not is_enrolled and user.is_authenticated:
+
+        return HttpResponse(status=404) 
+
+    enrollment = Enrollment.objects.get(user=user, course=course)
+    submission = Submission.objects.create(enrollment=enrollment)
+    extracted_answers = extract_answers(request)
+
+    for extracted_answer in extracted_answers:
+        choice = get_object_or_404(Choice, pk=extracted_answer)
+        submission.choices.add(choice)
+    submission.save()
+    
+    return HttpResponseRedirect(reverse(viewname='onlinecourse:show_exam_result', args=(course.id, submission.id)))
+
+
+
+
 
 
 # <HINT> A example method to collect the selected choices from the exam form from the request object
-#def extract_answers(request):
-#    submitted_anwsers = []
-#    for key in request.POST:
-#        if key.startswith('choice'):
-#            value = request.POST[key]
-#            choice_id = int(value)
-#            submitted_anwsers.append(choice_id)
-#    return submitted_anwsers
+def extract_answers(request):
+    submitted_anwsers = []
+    for key in request.POST:
+        if key.startswith('choice'):
+            value = request.POST[key]
+            choice_id = int(value)
+            submitted_anwsers.append(choice_id)
+    return submitted_anwsers
 
 
 # <HINT> Create an exam result view to check if learner passed exam and show their question results and result for each question,
@@ -130,7 +152,39 @@ def enroll(request, course_id):
         # Get the selected choice ids from the submission record
         # For each selected choice, check if it is a correct answer or not
         # Calculate the total score
-#def show_exam_result(request, course_id, submission_id):
+def show_exam_result(request, course_id, submission_id):
+    user = request.user
+    course = get_object_or_404(Course, pk=course_id)
+
+    is_enrolled = check_if_enrolled(user, course)
+    if not is_enrolled and user.is_authenticated:
+
+        return HttpResponse(status=404)
+
+    submission = get_object_or_404(Submission, pk=submission_id)
+    total_score = 0
+    lessons = course.lesson_set.all()
+    questions = []
+    for lesson in lessons:
+        lesson_questions = lesson.question_set.all()
+        for lesson_question in lesson_questions:
+            questions.append(lesson_question) 
+    
+    for question in questions:
+        correct_user_answers = submission.choices.filter(indicator=True).count()
+        correct_choices = question.choice_set.filter(indicator=True).count()
+        choice_grade = question.grade//correct_choices if correct_choices else 0
+        total_score += choice_grade*correct_user_answers
+        print(choice_grade*correct_user_answers, correct_user_answers, correct_choices, choice_grade)
+           
+
+
+    selected_ids = [choice.id for choice in submission.choices.all()]
+    context = {'course':course, 'selected_ids':selected_ids , 'grade':total_score}
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
+
+
+
 
 
 
